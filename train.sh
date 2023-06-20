fd4c6e52a31bf5df4a27d63453a3687e47d35478
Brian Zhao <briannlz@usc.edu>
2023-06-20 02:36:27 -0700

    update dataset path

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
 export CUDA_VISIBLE_DEVICES="0,1,2,3";
 accelerate launch --mixed_precision=bf16 --multi_gpu train.py \
-    --train_dir imagenet_syn_100img100cls_no_noise \
-    --val_dir val \
-    --test_dir val \
+    --train_dir datasets/imagenet_syn_100img100cls_no_noise \
+    --val_dir datasets/val \
+    --test_dir datasets/val \
     --model resnet50 \
-    --aa v0 --mixup 0.2 -b 512
+    --aa v0 \
+    --mixup 0.2 \
+    -b 128
