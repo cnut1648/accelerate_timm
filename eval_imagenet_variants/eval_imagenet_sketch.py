986187b54788acd5b7d9fe9f77dcdf554732dc18
Brian Zhao <briannlz@usc.edu>
2023-10-28 06:51:31 -0700

    100cls for sketch

--- eval_imagenet_variants/eval_imagenet_sketch.py
+++ eval_imagenet_variants/eval_imagenet_sketch.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import sys
 import PIL
 import torch
 import torchvision
@@ -8,6 +8,9 @@ from tqdm.auto import tqdm
 from utils import get_args, load_model
 
 pwd = Path(__file__).parent.resolve()
+sys.path.insert(0, str(pwd.parent))
+from classnames_imagenet import subset100
+from classnames_imagenet import classnames_simple as classnames
 
 
 class ImagenetSketch(torch.utils.data.Dataset):
@@ -36,16 +39,34 @@ class ImagenetSketch(torch.utils.data.Dataset):
         return image, label
 
 @torch.no_grad()
-def evaluate(
-    model, test_dataloader
-):
+def evaluate(model, test_dataloader, n_cls):
+    if n_cls == 100:
+        thousand_wnid = sorted(list(classnames.keys()))
+        hundred_wnid = sorted(subset100)
+        thousand2hundred = {}
+        for i in range(1000):
+            if thousand_wnid[i] in hundred_wnid:
+                thousand2hundred[i] = hundred_wnid.index(thousand_wnid[i])
+    elif n_cls == 1000:
+        pass
+    else:
+        raise ValueError("n_cls should be either 100 or 1000")
+
     model.eval().cuda()
-    top1 = torchmetrics.Accuracy(task="multiclass", num_classes=1000, top_k=1).to("cuda")
-    top5 = torchmetrics.Accuracy(task="multiclass", num_classes=1000, top_k=5).to("cuda")
+    top1 = torchmetrics.Accuracy(task="multiclass", num_classes=n_cls, top_k=1).to("cuda")
+    top5 = torchmetrics.Accuracy(task="multiclass", num_classes=n_cls, top_k=5).to("cuda")
     for step, (inputs, targets) in tqdm(enumerate(test_dataloader), total=len(test_dataloader)):
-        inputs, targets = inputs.cuda(), targets.cuda()
+        inputs = inputs.cuda()
         outputs = model(inputs)
+        if n_cls == 100:
+            targets.apply_(lambda x: thousand2hundred.get(x, -1))
+            keep_indices = (targets != -1).nonzero(as_tuple=True)[0]
+            targets = targets[keep_indices]
+            outputs = outputs[keep_indices]
+            if len(targets) == 0:
+                continue
         preds = torch.argmax(outputs, dim=1)
+        targets = targets.cuda()
         top1.update(preds, targets)
         top5.update(outputs, targets)
 
@@ -61,4 +82,4 @@ if __name__ == "__main__":
     val_set = ImagenetSketch()
     test_dataloader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-    evaluate(model, test_dataloader)
+    evaluate(model, test_dataloader, n_cls=args.num_classes)
