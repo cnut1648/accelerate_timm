78743f4ec9b21e8369cfcbdf346a5098d00c0c55
Brian Zhao <briannlz@usc.edu>
2023-11-05 20:54:49 -0800

    training script

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES="0";
+export CUDA_VISIBLE_DEVICES="0,1";
 accelerate launch --mixed_precision=fp16 --multi_gpu train.py \
     --dset imagenet_syn \
-    --train_dir /lab/tmpig7b/u/brian-data/imagenet_syn_100img_reparam_ortho \
+    --train_dir /lab/tmpig7b/u/brian-data/imagenet_syn_fake_it \
     --val_dir /lab/tmpig7b/u/brian-data/imagenet/images/val \
     --test_dir /lab/tmpig7b/u/brian-data/imagenet/images/val \
     --model resnet50 \
@@ -9,5 +9,5 @@ accelerate launch --mixed_precision=fp16 --multi_gpu train.py \
     --aa v0 \
     --mixup 0.2 \
     --epochs 300 \
-    --output_dir output/100img_reparam_ortho \
-    -b 128 
+    --output_dir output/fake_it \
+    -b 256
