d5fe05ebc1f020cf1d3a092a1e17fdf29c22c127
Brian Zhao <briannlz@usc.edu>
2024-01-30 21:25:41 -0800

    train mix imagenet script

--- add_caption_dir.py
+++ add_caption_dir.py
@@ -2,14 +2,16 @@ import os
 import sys
 from tqdm import tqdm
 import shutil
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--root_dir", type=str, required=True)
+args = parser.parse_args()
 
-root_dir = "/lab/tmpig7b/u/brian-data/dreambooth_medium"
-
-for class_dir in tqdm(os.listdir(root_dir)):
-    os.makedirs(os.path.join(root_dir, class_dir, class_dir), exist_ok=True)
-    for img in os.listdir(os.path.join(root_dir, class_dir)):
+for class_dir in tqdm(os.listdir(args.root_dir)):
+    os.makedirs(os.path.join(args.root_dir, class_dir, class_dir), exist_ok=True)
+    for img in os.listdir(os.path.join(args.root_dir, class_dir)):
         if img.endswith("png"):
-            src = os.path.join(root_dir, class_dir, img)
-            dst = os.path.join(root_dir, class_dir, class_dir, img)
+            src = os.path.join(args.root_dir, class_dir, img)
+            dst = os.path.join(args.root_dir, class_dir, class_dir, img)
             shutil.move(src, dst)
